eb4d385c583360f15ce5b47a109b317cfb76995d
samairak123 <samairak@nycstudents.net>
2025-10-03 12:49:42 -0400

    october 3rd

---
+++ script.js
@@ -0,0 +1,6 @@
+let activities = [
+    {place: "foreign country", activity: eat new food},
+    {place: "zoo", activity: feed gorillas},
+    {place: "karaoke", activity: sing with friends},
+    {place: "city", activity: go shopping}
+]
